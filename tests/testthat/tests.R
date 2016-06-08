576cc3a891aabf6c95a8bd662b22fa13df31c52e
Andrew Marder <andrew.n.marder@gmail.com>
2016-06-08 13:03:00 -0400

    Modified test to get a little bit of coverage.

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -1,5 +1,5 @@
 test_that("compute visitors from actions", {
     actions <- data.frame()
-    ## visitors <- compute_visitors(actions)
     expect_that(1 + 1, equals(2))
+    expect_that(compute_visitors(actions), throws_error())
 })
